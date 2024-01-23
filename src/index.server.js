6fb36856541fabf57ec52237d2d6c150378f6856
wannabefrontdeveloper <giwonk@naver.com>
2024-01-23 12:37:33 +0900

    코드추가 asset-manifest.json 파일의 main.css와 main.js 파일을 html 내부에 삽입하도록 하였습니다.

--- src/index.server.js
+++ src/index.server.js
@@ -3,6 +3,35 @@ import express from "express";
 import { StaticRouter } from "react-router-dom/server";
 import App from "./App";
 import path from "path";
+import fs from "fs";
+
+// asset-manifest.json에서 파일 경로들을 조회합니다.
+const manifest = JSON.parse(
+  fs.readFileSync(path.resolve("./build/asset-manifest.json"), "utf8")
+);
+
+function createPage(root) {
+  return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+  <meta charset="utf-8" />
+  <link rel="shortcut icon" href="/favicon.ico" />
+  <meta 
+  name="viewport"
+  content="width=device-width,initial-scale=1, shrink-to-fit=no"
+  />
+  <meta name="theme-color" content="#000000" />
+  <title>React App</title>
+  <link href="${manifest.files["main.css"]}" rel="stylesheet" />
+  </head>
+  <body>
+  <noscript>You need to enable JavaScript to run this app.</noscript>
+  <div id="root">${root}</div>
+  <script src="${manifest.files["main.js"]}"></script>
+  </body>
+  </html>
+  `;
+}
 
 const app = express();
 
@@ -17,7 +46,7 @@ const serverRender = (req, res, next) => {
     </StaticRouter>
   );
   const root = ReactDOMServer.renderToString(jsx); // 렌더링을 하고
-  res.send(root); // 클라이언트에게 결과물을 응답합니다.
+  res.send(createPage(root)); // 클라이언트에게 결과물을 응답합니다.
 };
 
 const serve = express.static(path.resolve("./build"), {
