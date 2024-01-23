eeddfba08377c4d74123ad1f4a9878491607cfb9
wannabefrontdeveloper <giwonk@naver.com>
2024-01-23 13:02:09 +0900

    코드 생성 사용자에 대한 정보를 보여 줄 컴포넌트입니다.

---
+++ src/containers/UsersContainer.js
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import Users from "../components/Users";
+import { useDispatch, useSelector } from "react-redux";
+import { getUsers } from "../modules/users";
+const UsersContainer = () => {
+  const users = useSelector((state) => state.users.users);
+  const dispatch = useDispatch();
+
+  //컴포넌트가 마운트되고 나서 호출
+  useEffect(() => {
+    if (users) return; // users가 이미 유효하다면 요청하지 않음
+    dispatch(getUsers());
+  }, [dispatch, users]);
+
+  return <Users users={users} />;
+};
+
+export default UsersContainer;
