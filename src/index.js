c7ceca75fc95513753f990cda44915c43341871b
wannabefrontdeveloper <giwonk@naver.com>
2024-01-23 13:12:12 +0900

    오류 수정

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-import thunk from "redux-thunk";
+import { thunk } from "redux-thunk";
 import rootReducer from "./modules";
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
