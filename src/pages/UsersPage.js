d76a335e42e8ab9f30ce449f2b084c5d776dd498
wannabefrontdeveloper <giwonk@naver.com>
2024-01-24 10:46:57 +0900

    코드 수정 Route에 component 대신 render를 설정해주었습니다.

--- src/pages/UsersPage.js
+++ src/pages/UsersPage.js
@@ -1,7 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import UsersContainer from "../containers/UsersContainer";
+import UserPage from "./UserPage";
 
 const UsersPage = () => {
-  return <UsersContainer />;
+  return (
+    <>
+      <UsersContainer />
+      <Routes>
+        <Route path=":id" element={<UserPage />} />
+      </Routes>
+    </>
+  );
 };
 
 export default UsersPage;
