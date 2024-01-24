0b7be83d14a5f70c4ef416b11a02d1e80d08c212
wannabefrontdeveloper <giwonk@naver.com>
2024-01-24 09:34:52 +0900

    코드수정 Preloader 컴포넌트를 사용하도록 하였습니다.

--- src/containers/UsersContainer.js
+++ src/containers/UsersContainer.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import Users from "../components/Users";
 import { useDispatch, useSelector } from "react-redux";
 import { getUsers } from "../modules/users";
+import { Preloader } from "../lib/PreloadContext";
 const UsersContainer = () => {
   const users = useSelector((state) => state.users.users);
   const dispatch = useDispatch();
@@ -12,7 +13,12 @@ const UsersContainer = () => {
     dispatch(getUsers());
   }, [dispatch, users]);
 
-  return <Users users={users} />;
+  return (
+    <>
+      <Users users={users} />
+      <Preloader resolve={() => dispatch(getUsers)} />
+    </>
+  );
 };
 
 export default UsersContainer;
